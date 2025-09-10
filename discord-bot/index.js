8ff773aa183fd8dc41f333e7ee8f7cd61979abd3
Sahil Silare <sahilsilare@gmail.com>
2025-09-10 23:58:58 +0530

    feat: added discord bot
    Signed-off-by: Sahil Silare <sahilsilare@gmail.com>

---
+++ discord-bot/index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    if (message.content.startsWith('create')) {
+        const url = message.content.split('create')[1];
+        return message.reply({
+            content: "Generating short id for" + url
+        })
+    }
+    message.reply({
+        content: "Hi from bot!"
+    })
+});
+
+client.on("interactionCreate", (interaction) => {
+    interaction.reply("Pong!!");
+})
+
+client.login('token');
